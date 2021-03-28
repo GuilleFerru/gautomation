2b8fea7cb16907857214361dca98741449291b4d
GuilleFerru <guillelf@gmail.com>
2021-03-28 16:52:36 -0300

    navbar responsive potable

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 
 
-$(document).scroll(function(){
+$(document).scroll(function () {
 	$('.fixedContainer,.header__nav--scroll,.header__logo--scroll').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 
-
+$('.navbar-collapse li').click(function(){
+	$(".navbar-collapse").collapse('hide');
+});
 
 
